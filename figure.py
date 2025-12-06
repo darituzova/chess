703bee9a57d60e9bd717cd10b1d4ecc9d27ca53a
Daria Tuzova <darituzova@yandex.ru>
2025-12-06 18:01:09 +0300

    Update figure.py

--- figure.py
+++ figure.py
@@ -9,21 +9,10 @@ class Figure:
         self.position = position
         self.figure_type = figure_type.lower()
         self.has_moved = False
-
+    
     # Возможные ходы (в дочерних классах)
     def get_correct_moves(self, board):
         pass
-    
-    # Проверка возможности хода в указанную позицию
-    def can_move(self, to_position, board):
-        # Используем get_correct_moves из дочернего класса
-        possible_moves = self.get_correct_moves(board)
-        return to_position in possible_moves
-    
-    # Обновление позиции фигуры и отметка о движении
-    def move(self, to_position):
-        self.position = to_position
-        self.has_moved = True
     
     # Возвращение типа фигуры
     def get_type(self):
